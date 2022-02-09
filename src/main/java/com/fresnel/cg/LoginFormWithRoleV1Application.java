bcb662e97f4fde3166e4c73cb705e235d4fc04dc
billfres <ngomafresnel@gmail.com>
2022-02-09 05:04:27 +0100

    Configuration de application.propreties

---
+++ src/main/java/com/fresnel/cg/LoginFormWithRoleV1Application.java
@@ -0,0 +1,13 @@
+package com.fresnel.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFormWithRoleV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginFormWithRoleV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fresnel/cg/LoginFormWithRoleV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fresnel.cg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginFormWithRoleV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
